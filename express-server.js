35ece4f5f2d56eb2ffed7b05f60ed78dbcc08282
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-02-13 16:58:27 -0700

    fixed urls_index.ejs (presentation still rough)

--- express-server.js
+++ express-server.js
@@ -9,13 +9,17 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/urls', (req, res) => {
-    let templateVars = {urls: urlDatabase};
-    res.render('urls_index', templateVars);
-});
+app.get("/", (req, res) => {
+  res.send('Hello!')
+})
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/urls', (req, res) => {
+    let templateVars = {urls: urlDatabase};
+    res.render('urls_index', templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -24,11 +28,11 @@ app.get("/urls/:shortURL", (req, res) => {
       longURL: urlDatabase[req.params.id]
     };
     res.render("urls_show", templateVars);
-  });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
