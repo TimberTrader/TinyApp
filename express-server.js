68fd97b6927e60d16c0045f574d48d24b550bd3d
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-02-13 15:13:36 -0700

    added get(urls.json) & partials

--- express-server.js
+++ express-server.js
@@ -14,6 +14,10 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
     let templateVars = {
       shortURL: req.params.shortURL,
