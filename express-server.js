22d4d693f8d6e90741f9a68f74aec5e01eb83304
Tim Bowes <timber.trader@Tims-MacBook-Pro.local>
2019-02-14 15:15:35 -0700

    comple funct for deleting urls {added delete button & delete route} need presentation cleanup

--- express-server.js
+++ express-server.js
@@ -1,15 +1,25 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs')
+
+function generateRandomString() {
+  let result = '';
+  let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  for (var i = 6; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+  return result;
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello!')
 })
 
@@ -17,22 +27,38 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls', (req, res) => {
     let templateVars = {urls: urlDatabase};
     res.render('urls_index', templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
     let templateVars = {
       shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL] 
+      longURL: urlDatabase[req.params.shortURL]
     };
     res.render("urls_show", templateVars);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.post('/urls', (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL
+  urlDatabase[shortURL] = {
+    shortURL: shortURL,
+    longURL: longURL
+  }
+  res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls/');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
